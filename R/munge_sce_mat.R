54dbed48efcba4773dc138347b4dd9e82e98fa83
Qiliang Lai <ql29@risotto.cs.rice.edu>
2023-08-16 10:04:17 -0500

    fix bugs in munge_sce_mat()

--- R/munge_sce_mat.R
+++ R/munge_sce_mat.R
@@ -1,12 +1,15 @@
-#' This function maps feature (gene) names to either entrez or other format, similar to `trans_mmu_to_hsa_stat`.
+#' Map feature (gene) names (row names) to either entrez or other format
+#' 
+#' This function is similar to `trans_mmu_to_hsa_stat`.
 #' A new SingleCellExperiment object will be created, with cell meta data the same as the original 
 #' The gene meta data will be discarded
 #'
 #' @param data_obj SingleCellExperiment object
 #' @param assay_type name of the assay for the mapping. By default it will be all assays. 
-#' @param mapping_df A data frame to specify the mapping. The first column should contain all the featyre names while the second column indicates the new feature names to be mapped to.
+#' @param mapping_df A data frame to specify the mapping. The first column should contain all the feature names while the second column indicates the new feature names to be mapped to.
 #' @param multi_mapping How to translate feature name when multiple mapping exists?? By "mean" value or "sum" value? (sum for default)
 #' @return A SingleCellExperiment object 
+#' @importFrom S4Vectors DataFrame
 #' @export
 #' 
 munge_sce_mat = function(data_obj,  mapping_df, assay_type = "all",multi_mapping = "sum") {
@@ -25,6 +28,7 @@ munge_sce_mat = function(data_obj,  mapping_df, assay_type = "all",multi_mapping
   #merge
   new_assay = list()
   mapping_df = mapping_df %>% mutate_all(~as.character(.))
+  
   for (assay_name in assay_type){
     data_mat = SummarizedExperiment::assay(data_obj, assay_name)
     #filter mapping
@@ -35,9 +39,10 @@ munge_sce_mat = function(data_obj,  mapping_df, assay_type = "all",multi_mapping
     data_mat = data_mat[match(all_mapping[[1]],rownames(data_mat)),]
     #split matrix into two
     multiple_mapping = all_mapping %>% 
-      group_by(.[[2]]) %>%
+      group_by_at(2) %>%
       filter(n()>=2) %>%
       ungroup
+    #separate the matrix with multiple mapping a
     single_mat = data_mat[which(!rownames(data_mat) %in% multiple_mapping[[1]]),]
     multi_mat = data_mat[which(rownames(data_mat) %in% multiple_mapping[[1]]),]
     
@@ -46,11 +51,15 @@ munge_sce_mat = function(data_obj,  mapping_df, assay_type = "all",multi_mapping
     if(multi_mapping=="mean"){
       fac_mat = sweep_sparse(fac_mat, margin=1, stats = Matrix::rowSums(fac_mat), fun = "/")
     }
+    #merge and set rownames
     multi_mat = ( fac_mat %*% multi_mat) %>% 
       magrittr::set_rownames(unique(multiple_mapping[[2]])) %>%
       magrittr::set_colnames(colnames(data_mat))
-    single_mat = single_mat %>% magrittr::set_rownames(all_mapping %>% filter(! .[[1]] %in% multiple_mapping[[1]] ) %>% pull(1))
+    single_mat = single_mat %>% magrittr::set_rownames(all_mapping %>% filter(! .[[1]] %in% multiple_mapping[[1]] ) %>% pull(2))
     data_mat = rbind(single_mat, multi_mat)
+    #rearrange based on the new gene names
+    data_mat = data_mat[match(unique(all_mapping[[2]]),rownames(data_mat)),]
+    #add assay
     new_assay = new_assay %>% append(data_mat)
   }
   new_assay = new_assay %>% purrr::set_names(assay_type)
@@ -59,7 +68,9 @@ munge_sce_mat = function(data_obj,  mapping_df, assay_type = "all",multi_mapping
   out_data = SingleCellExperiment::SingleCellExperiment(
     assays = new_assay,
     colData = SummarizedExperiment::colData(data_obj),
-    rowData = data.frame(rownames(new_assay[[1]]), row.names = rownames(new_assay[[1]]),fix.empty.names = FALSE) %>% magrittr::set_colnames(colnames(mapping_df)[2])
+    rowData = data.frame(rownames(new_assay[[1]]), row.names = rownames(new_assay[[1]]),fix.empty.names = FALSE) %>% 
+      magrittr::set_colnames(colnames(mapping_df)[2]) %>%
+      S4Vectors::DataFrame()
   )
   
   return(out_data)

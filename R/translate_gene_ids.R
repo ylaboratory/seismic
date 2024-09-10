b541ff9b5bc23d1a615b94b720bfdb6f68b4f930
VicentQLai <ql29@rice.edu>
2024-09-10 14:56:34 -0500

    adapted translate_gene_ids() for more data type

--- R/translate_gene_ids.R
+++ R/translate_gene_ids.R
@@ -16,7 +16,7 @@
 #'
 translate_gene_ids <- function(sscore, from = "mmu_ensembl", to = "hsa_entrez", multi_mapping = "mean") {
   # check input type
-  if (!class(sscore)[1] %in% c("dgeMatrix", "matrix")) {
+  if (!class(sscore)[1] %in% c("dgeMatrix","dgCMatrix", "matrix")) {
     stop("This function is to be used directly with the direct output of calc_specificity(). The data is not an acceptable matrix type.")
   }
 
